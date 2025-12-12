e93393dddb64937aeb8e61b7f5489c1310d9111e
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-12-12 21:21:33 +0500

    Update the code

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -19,7 +19,7 @@ const Card = ({ name, flag, population, region, capital }) => {
 
   
       <div className="p-4 space-y-1 ">
-        <h1 className="text-md font-semibold">{name}</h1>
+        <h1 className="text-md font-semibold ">{name}</h1>
         <p className="text-sm opacity-80"><span className="font-medium">Region:</span> {region}</p>
         <p className="text-sm opacity-80"><span className="font-medium">Capital:</span> {capital}</p>
         <p className="text-sm opacity-80"><span className="font-medium">Population:</span> {population.toLocaleString()}</p>
